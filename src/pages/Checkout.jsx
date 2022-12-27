b7a24fbc6b31f7f9f8ba4f3e652b929c58a245d6
kingorianderson <kingorianderson@gmail.com>
2022-12-27 17:01:41 +0300

    Ksh.

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -102,14 +102,14 @@ const Checkout = () => {
             <Col lg="4" md="6">
               <div className="checkout__bill">
                 <h6 className="d-flex align-items-center justify-content-between mb-3">
-                  Subtotal: <span>${cartTotalAmount}</span>
+                  Subtotal: <span>Ksh.{cartTotalAmount}</span>
                 </h6>
                 <h6 className="d-flex align-items-center justify-content-between mb-3">
-                  Shipping: <span>${shippingCost}</span>
+                  Shipping: <span>Ksh.{shippingCost}</span>
                 </h6>
                 <div className="checkout__total">
                   <h5 className="d-flex align-items-center justify-content-between">
-                    Total: <span>${totalAmount}</span>
+                    Total: <span>Ksh.{totalAmount}</span>
                   </h5>
                 </div>
               </div>
